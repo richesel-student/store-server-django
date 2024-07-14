2562b664023225b221ae7d0cc333d1634969e0b5
Richese Lync <richesel@at-d2.msk.21-school.ru>
2024-07-14 21:08:15 +0300

    save_migration

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    discription = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    discription = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+
 # Create your models here.
