ef97df1b3894608323f83368042451634a84c0b9
Richese Lync <richesel@at-e3.msk.21-school.ru>
2024-08-31 12:57:45 +0300

    authorization and registration

--- store-server/store/users/forms.py
+++ store-server/store/users/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import  AuthenticationForm
 
 from users.models import User
 
-class UserLoginForum(AuthenticationForm):
+class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
 from users.models import User
-from users.forms import UserLoginForum
+from users.forms import UserLoginForm
+from django.urls import reverse
 
 def login(reqest):
-    context = {'form': UserLoginForum()}
+    if reqest.method == 'POST':
+        form = UserLoginForm(data=reqest.POST)
+        if form.is_valid():
+            username = reqest.POST['username']
+            password = reqest.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(reqest, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(reqest, 'users/login.html', context)
 
 def registration(reqest):
