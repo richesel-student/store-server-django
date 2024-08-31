8c78a973849b535c4c9ea0d1dbe94d1c218bd164
Richese Lync <richesel@at-e3.msk.21-school.ru>
2024-08-31 10:32:27 +0300

    work with authorization and registration

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def login(reqest):
+    return render(reqest, 'users/login.html')
+
+def registration(reqest):
+    return render(reqest, 'users/registration.html')
+
 # Create your views here.
