37588027e87b4cbefcefaf4262fb981ef127d647
Richese Lync <richesel@at-e3.msk.21-school.ru>
2024-09-01 17:48:47 +0300

    authorization err

--- store-server/store/users/forms.py
+++ store-server/store/users/forms.py
@@ -1,9 +1,6 @@
 
 from django import forms
-from django.contrib.auth.forms import  AuthenticationForm, UserCreationForm
-
-from users.models import User
-
+from django.contrib.auth.forms import  AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -14,21 +11,35 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
 class  UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя '}))
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={
+    email = forms.CharField(widget=forms.EmailInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
-    password1 = forms.CharField(widget=forms.TextInput(attrs={
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
-    password2 = forms.CharField(widget=forms.TextInput(attrs={
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Подтверждение пароля'}))
-
-
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username','email', 'password1', 'password2')
+
+
+
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control  py-4'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control  py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control  py-4', 'readonly': True}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control  py-4', 'readonly': True}))
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
+
 
 
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -3,26 +3,51 @@ from django.contrib import auth
 from users.models import User
 from users.forms import UserLoginForm
 from django.urls import reverse
-from users.forms import  UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
-def login(reqest):
-    if reqest.method == 'POST':
-        form = UserLoginForm(data=reqest.POST)
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = reqest.POST['username']
-            password = reqest.POST['password']
+            username = request.POST['username']
+            password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
             if user:
-                auth.login(reqest, user)
+                auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
     else:
         form = UserLoginForm()
     context = {'form': form}
-    return render(reqest, 'users/login.html', context)
+    return render(request, 'users/login.html', context)
 
-def registration(reqest):
-    form = UserRegistrationForm()
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
     context = {'form': form}
-    return render(reqest, 'users/registration.html', context)
+    return render(request, 'users/registration.html', context)
+
+#
+def profile(request):
+    context = {'title': 'Store - Профиль'}
+    return render(request, 'users/profile.html', context)
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('user:login'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserRegistrationForm()
+#     context = {'tiatle': 'Store - Профиль', 'form': form}
+#     return render(request, 'users/profile.html', context)
+
 
 # Create your views here.
