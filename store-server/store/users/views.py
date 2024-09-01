1b3ff39ddbe2665b5ddce6298ccf79cd8b3f89f6
Richese Lync <richesel@at-e3.msk.21-school.ru>
2024-09-01 08:12:48 +0300

    work with registration

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from users.models import User
 from users.forms import UserLoginForm
 from django.urls import reverse
+from users.forms import  UserLoginForm, UserRegistrationForm
 
 def login(reqest):
     if reqest.method == 'POST':
@@ -20,6 +21,8 @@ def login(reqest):
     return render(reqest, 'users/login.html', context)
 
 def registration(reqest):
-    return render(reqest, 'users/registration.html')
+    form = UserRegistrationForm()
+    context = {'form': form}
+    return render(reqest, 'users/registration.html', context)
 
 # Create your views here.
